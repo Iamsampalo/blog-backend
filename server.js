188244515fd8408a27ed1574cf65a69e4fb64e22
Iamsampalo <samueldesinger32@gmail.com>
2026-07-01 13:58:51 +0100

    added cors origin to server.js to allow requests from localhost:5173

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import commentsRoutes from './routes/commentsRoutes.js'
 import repostRoutes from './routes/repostRoutes.js'
 import authorize from './middlewares/authorize.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3700;
@@ -20,6 +21,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("Connected to MongoDB"))
